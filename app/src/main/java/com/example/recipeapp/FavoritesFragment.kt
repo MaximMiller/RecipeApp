8d08e9f4a6088733b2a6a667e03fc074d2d4ca64
Maxim <55555lab@mail.ru>
2024-07-12 18:10:40 +0300

    RA-04 - fix

---
+++ app/src/main/java/com/example/recipeapp/FavoritesFragment.kt
@@ -0,0 +1,30 @@
+package com.example.recipeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.recipeapp.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment:Fragment() {
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalAccessException("Binding for FragmentFavoritesBinding most not be null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
