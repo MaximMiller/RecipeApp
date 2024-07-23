ea88bef6ecc2a842a06328a68351119e8361cf06
Maxim <55555lab@mail.ru>
2024-07-23 13:46:50 +0300

    RA-07 - fix

--- app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -1,13 +1,16 @@
 package com.example.recipeapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.recipeapp.databinding.FragmentListCategoriesBinding
 
 class CategoriesListFragment : Fragment() {
+    private var currentCategory: String = "Default Category"
     private var _binding: FragmentListCategoriesBinding? = null
     private val binding
         get() = _binding
