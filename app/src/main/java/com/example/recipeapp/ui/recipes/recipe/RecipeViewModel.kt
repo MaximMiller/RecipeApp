9683d7841d8166826994b8ab5ca2ee08d7e1e49a
Maxim <55555lab@mail.ru>
2024-11-19 20:58:03 +0300

    RA_22 complete

--- app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,9 +1,21 @@
 package com.example.recipeapp.ui.recipes.recipe
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.recipeapp.model.Recipe
 
 class RecipeViewModel : ViewModel() {
+
+    private val _recipeState = MutableLiveData<RecipeState>()
+    val recipeState: LiveData<RecipeState>
+        get() = _recipeState
+
+    init {
+        Log.i("tag", "RecipeViewModel init")
+        _recipeState.value = RecipeState(isFavorite = true)
+    }
 
     data class RecipeState(
         val recipe: Recipe? = null,
